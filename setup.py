8c02594308ac4d1ee6ce14918d8f0542763bba5d
Bernie van Veen <b.vanveen@vwt.digital>
2019-04-15 21:29:04 +0200

    Fixed license classifier. CAR-29

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
      ],
      classifiers=[
          "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: GPLv3 License",
+         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
          "Operating System :: OS Independent",
      ],
  )
