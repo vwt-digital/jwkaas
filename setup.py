87b24c860e07ff1a2b1f5fe1b5d134f012b9b13d
Eric van der Velde <e.vandervelde@vwt.digital>
2021-02-10 13:02:29 +0100

    Added cloudbuild. DAT-5687

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
-import setuptools
+import os
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from setuptools import setup, find_packages
 
-setuptools.setup(
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+setup(
      name='jwkaas',
-     version='0.7',
+     packages=find_packages(),
+     include_package_data=True,
+     version=os.getenv('TAG_NAME', '0.0.0'),
+     license='gpl-3.0',
      scripts=[],
+     description="JSON Web Key Advanced Acquiring Store",
+     long_description=open('README.md').read(),
+     long_description_content_type="text/markdown",
      author="Bernie van Veen",
      author_email="b.vanveen@vwt.digital",
-     description="JSON Web Key Advanced Acquiring Store",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
      url="https://github.com/vwt-digital/jwkaas",
-     packages=setuptools.find_packages(),
-     install_requires=[
-         'pyjwt==1.7.1',
-         'cryptography==3.2'
-     ],
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
          "Operating System :: OS Independent",
      ],
+     install_requires=install_requires,
+     python_requires='>=3.6',
  )
